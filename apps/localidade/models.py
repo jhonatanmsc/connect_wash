023ed8d73bf637635a02e8f7e5a928c2cbba8d8a
jhonatanmsc <jhonatanmatheus80@gmail.com>
2018-12-07 23:37:38 -0300

    add models basicos de localidade e usuario

--- apps/localidade/models.py
+++ apps/localidade/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from utils.localidade import *
+
+
+class Endereco(models.Model):
+    cep = models.CharField(verbose_name='CEP', max_length=9)
+    cidade = models.ForeignKey('Cidade', models.DO_NOTHING, null=True)
+    bairro = models.ForeignKey('Bairro', models.DO_NOTHING, null=True)
+    logradouro = models.CharField(verbose_name='Logradouro', max_length=100, null=True)
+    numero = models.CharField(verbose_name='Número', max_length=10, blank=True, null=True)
+    complemento = models.CharField(max_length=100, blank=True, null=True)
+    referencia = models.CharField(verbose_name='Referência', max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+
+    def __str__(self):
+        return '%s, %s' % (self.bairro.nome, self.cidade)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    uf = models.CharField(max_length=2, choices=UFs, verbose_name="UF")
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = 'Cidades'
+        ordering = ['nome']
+
+    def __str__(self):
+        return '%s %s' % (self.nome, self.uf)
+
+
+class Bairro(models.Model):
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    cidade = models.ForeignKey('Cidade', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Bairro'
+        verbose_name_plural = 'Bairros'
+        ordering = ['nome']
+
+    def __str__(self):
+        return '%s %s' % (self.nome, self.cidade)
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,88 @@
-from django.db import models
-
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import Permission
+from apps.localidade.models import Endereco
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, nome, password=None):
+        if not email:
+            raise ValueError('Usuarios devem ter cpfs validos!')
+
+        user = self.model(
+            cpf_cnpj=self.normalize_email(email),
+            nome=nome,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nome, password):
+        user = self.create_user(
+            email,
+            password=password,
+            nome=nome,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Perfil(models.Model):
+    tipo = models.CharField('cargo', max_length=100)
+    permissoes = models.ManyToManyField(
+        Permission,
+        verbose_name='Permissões',
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
+
+    def __str__(self):
+        return self.cargo
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    cpf_cnpj = models.CharField(u'cpf/cnpj', max_length=19, unique=True)
+    nome = models.CharField(u'nome', max_length=100)
+    email = models.EmailField(u'email', max_length=255, null=True, unique=True)
+    telefone = models.CharField('numero de telefone', max_length=30, null=True, blank=True)
+    data_nasc = models.DateField(null=True, blank=True)
+    adress = models.ForeignKey(Endereco, models.DO_NOTHING, null=True, blank=True)
+    perfil = models.ForeignKey(u'Perfil', models.DO_NOTHING,related_name='usuario', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        super(Usuario, self).save()
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    @property
+    def first_name(self):
+        return self.nome.split(' ')[0]
